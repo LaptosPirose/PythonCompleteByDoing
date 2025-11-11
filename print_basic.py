0c93373933cf5cc2d11ade3e61643c3464d98a88
Angelo Moura <angelomoura@gmail.com>
2025-11-11 16:43:16 -0300

    Refreshing app

---
+++ print_basic.py
@@ -0,0 +1,15 @@
+print(("Bob"+".") * 10)
+print(2j)
+
+print(10/2)
+
+print(14//2)
+
+message = """This is
+a multi-line
+String."""
+message = "this 'is' a message n' "
+
+print(message)
+print(message)
+print(message)
